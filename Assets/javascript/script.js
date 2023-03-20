302cb3ebb7c3726b916dd94cb98712576948ba1b
Reyn12 <muhammadrenaldi687@gmail.com>
2023-03-20 21:49:42 +0700

    Create Contact US Remake

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -29,3 +29,43 @@ btnKiri2.addEventListener("click", () => {
     card2[i].style.transform = "translateX(0px)";
   }
 });
+
+// ksahdusahydusa
+const cardcontact = document.querySelectorAll(".card-contact");
+const cardContents = document.querySelectorAll(".card-content");
+
+cardcontact.forEach((card, index) => {
+  card.addEventListener("click", function () {
+    cardcontact.forEach((c) => {
+      c.classList.remove("active");
+    });
+    card.classList.add("active");
+    cardContents.forEach((cardContent) => {
+      cardContent.classList.add("hidden");
+    });
+    cardContents[index].classList.toggle("hidden");
+  });
+});
+
+{
+  /* <div class="card-container">
+  <div class="card-contact contact-card">
+    <h2>Contact Card</h2>
+  </div>
+  <div class="card-contact maps-card">
+    <h2>Maps Card</h2>
+  </div>
+  <div class="card-contact faq-card">
+    <h2>FAQ Card</h2>
+  </div>
+</div>
+<div class="hidden card-content" id="contact">
+  <h3>INI card-contact CONTACT</h3>
+</div>
+<div class="hidden card-content" id="maps">
+  <h3>INI card-contact MAPS</h3>
+</div>
+<div class="hidden card-content" id="faq">
+  <h3>INI card-contact FAQ</h3>
+</div> */
+}
